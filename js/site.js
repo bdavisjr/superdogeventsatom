1a10052673369ba82a46361578717730caacbfef
Bobby Davis <bdavis@core-techs.net>
2024-07-17 14:08:42 -0400

    Initial commit

---
+++ js/site.js
@@ -0,0 +1,13 @@
+
+
+function getValues(){
+    alert("Hello App!!!");
+}
+
+//get values from the screen and display them
+function getValues2(){
+   let inputMessage = document.getElementById("message").value;  
+   let alertMessage = document.getElementById("alertMessage");    
+   alertMessage.innerText = inputMessage;
+
+}
